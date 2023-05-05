2120ea0538696159a9dfa4151bf3217f9defd9ae
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-05 10:27:24 -0400

    added fonts and making progress on sign in screen

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,15 @@
 import './Home.scss';
 import logo from "../../assets/logo-green.png"
 import SignIn from '../../components/SignIn/SignIn';
+import Button from '../../components/Button/Button'
 
 function Home (){
     return(
-        <section>
-            <div>
-                <img src={logo}alt="" />
+        <section className='home'>
+            <div className='home__container'>
+                <img src={logo} alt="ukg logo" />
                 <SignIn/>
+                <Button />
             </div>
         
         </section>
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,16 +2,22 @@ import "./SignIn.scss";
 
 function SignIn() {
     return (
-        <section>
-            <div>
+        <section className="signIn">
+            <div className="signIn__header">
                 <h2>Sign In</h2>
             </div>
-            <label htmlFor="">
-                <input type="text" placeholder="USERNAME"/>
-            </label>
-            <label htmlFor="">
-                <input type="password" placeholder="PASSWORD"/>
-            </label>
+            <div className="signIn__input-container">
+                <div>
+                    <label htmlFor="">
+                        <input type="text" placeholder="USERNAME" />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="">
+                        <input type="password" placeholder="PASSWORD" />
+                    </label>
+                </div>
+            </div>
         </section>
     );
 }
