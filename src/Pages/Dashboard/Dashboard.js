5059561e29ef96fa761a704a0603f565fe4c7142
Jorge Mantilla <33700227+manto465@users.noreply.github.com>
2023-05-05 12:28:35 -0400

    dashboard base

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Guy from '../../assets/guy .jpeg'
+import './Dashboard.scss'
+// import Navbar from '../../components/Navbar/Navbar'
+// import './Navbar.scss'
+
+const Dashboard = () => {
+  return (
+      <section className='dash'>
+        {/* <Navbar /> */}
+    <div>
+        <img src={Guy}  alt='headshot'/>
+        <h1>Hello Karim!</h1>
+        <p>Today is you 567th day at Pandora inc.</p>
+    </div>
+    </section>
+  )
+}
+
+export default Dashboard
