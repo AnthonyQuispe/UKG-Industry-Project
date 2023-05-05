8b1df50fa6c3b5a2e6ed4043a8744a3238acd77c
Jorge Mantilla <33700227+manto465@users.noreply.github.com>
2023-05-05 14:43:15 -0400

    tweaks tweakin

--- src/components/AchievementBox/AchievementBox.js
+++ src/components/AchievementBox/AchievementBox.js
@@ -1,59 +1,58 @@
 import React from "react";
 import "./AchievementBox.scss";
 import talentIcon from "../../assets/icons/Talent icon.png";
-import timeIcon from '../../assets/icons/Time Assesment icon.png';
-import reportIcon from '../../assets/icons/Reporting and analytics icon.png';
-import compIcon from '../../assets/icons/Compliance icon.png';
+import timeIcon from "../../assets/icons/Time Assesment icon.png";
+import reportIcon from "../../assets/icons/Reporting and analytics icon.png";
+import compIcon from "../../assets/icons/Compliance icon.png";
 
 const AchievementBox = () => {
   return (
     <article className="achieve">
-
-      <h1>Achievements this month:</h1>
-
-      <div className="achieve__card">
-      <div className="achieve__image-box">
-        <img src={talentIcon} alt="talent" className="achieve__image"></img>
-      </div>
-      <div className="achieve__text-box">
-        <p acheive__text>You participated in 2 company's events</p>
-      </div>
-      </div>
+      <h1 className="achieve__header">Achievements this month:</h1>
 
       <div className="achieve__card">
-      <div className="achieve__image-box">
-        <img src={timeIcon} alt="talent" className="achieve__image"></img>
-      </div>
-      <div className="achieve__text-box">
-        <p acheive__text>You participated in 2 company's events</p>
-      </div>
+        <div className="achieve__image-box">
+          <img src={talentIcon} alt="talent" className="achieve__image"></img>
+          <p className="achieve__text">
+            You participated in 10 company's events
+          </p>
+        </div>
       </div>
 
       <div className="achieve__card">
-      <div className="achieve__image-box">
-        <img src={reportIcon} alt="talent" className="achieve__image"></img>
-      </div>
-      <div className="achieve__text-box">
-        <p acheive__text>You participated in 2 company's events</p>
-      </div>
-      </div>
-
-      <div className="achieve__card">
-      <div className="achieve__image-box">
-        <img src={compIcon} alt="talent" className="achieve__image"></img>
-      </div>
-      <div className="achieve__text-box">
-        <p acheive__text>You participated in 2 company's events</p>
-      </div>
+        <div className="achieve__image-box">
+          <img src={timeIcon} alt="talent" className="achieve__image"></img>
+          <p className="achieve__text">
+            You participated in 2 company's events
+          </p>
+        </div>
       </div>
 
       <div className="achieve__card">
-      <div className="achieve__image-box">
-        <img src={talentIcon} alt="talent" className="achieve__image"></img>
+        <div className="achieve__image-box">
+          <img src={reportIcon} alt="talent" className="achieve__image"></img>
+          <p className="achieve__text">
+            You participated in 2 company's events
+          </p>
+        </div>
       </div>
-      <div className="achieve__text-box">
-        <p acheive__text>You participated in 2 company's events</p>
+
+      <div className="achieve__card">
+        <div className="achieve__image-box">
+          <img src={compIcon} alt="talent" className="achieve__image"></img>
+          <p className="achieve__text">
+            You participated in 2 company's events
+          </p>
+        </div>
       </div>
+
+      <div className="achieve__card">
+        <div className="achieve__image-box">
+          <img src={talentIcon} alt="talent" className="achieve__image"></img>
+          <p className="achieve__text">
+            You participated in 2 company's events
+          </p>
+        </div>
       </div>
     </article>
   );
