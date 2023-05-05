aeec84febe93e9a4444465cd43e79a3982c31e48
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-05 13:29:40 -0400

    dashboard work

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,20 +1,34 @@
 import React from "react";
 import Guy from "../../assets/Karim Abud.png";
 import "./Dashboard.scss";
-// import Navbar from '../../components/Navbar/Navbar'
-// import './Navbar.scss'
+import Navbar from "../../components/Navbar/Navbar";
+import PointsCard from "../../components/PointsCard/PointsCard"
+import AchievementBox from "../../components/AchievementBox/AchievementBox"
+import Moods from "../../components/Moods/Moods"
 
 const Dashboard = () => {
-  return (
-    <section className="dash">
-      {/* <Navbar /> */}
-      <div>
-        <img src={Guy} alt="headshot" />
-        <h1>Hello Karim!</h1>
-        <p>Today is you 567th day at Pandora inc.</p>
-      </div>
-    </section>
-  );
+    return (
+        <section className="dash">
+            <Navbar />
+            <div className="dash__content">
+                <div className="dash__content-container">
+                    <img src={Guy} alt="headshot" className="dash__image" />
+                </div>
+                <div className="dash__content-container">
+                    <h1>Hello Karim!</h1>
+                    <p>Today is you 567th day at Pandora inc.</p>
+                </div>
+                <div className="dash__moods">
+                  <Moods />
+                </div>
+                <div className="dash__cards-container">
+                  <PointsCard />
+                  <AchievementBox />
+                </div>
+                
+            </div>
+        </section>
+    );
 };
 
 export default Dashboard;
