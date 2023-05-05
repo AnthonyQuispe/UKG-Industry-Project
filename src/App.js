921b749ad4b069ad54c9d138a58388b7820e74fc
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-05-05 11:53:01 -0400

    finished styling the signin page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+// import Home from "./Pages/Home/Home";
+import Chatgpt from "./components/ChatGpt/Chatgpt";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Our Header will go here</header>
-      <Home/>
-    </div>
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/chat" element={<Chatgpt />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
