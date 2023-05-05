691a4b23b6966a2aae5486532d2d6ba153172d0d
Jorge Mantilla <33700227+manto465@users.noreply.github.com>
2023-05-05 11:23:53 -0400

    component structured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./Pages/Home/Home";
+import PointsCard from "./components/PointsCard/PointsCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Our Header will go here</header>
       <Home/>
+      <PointsCard />
     </div>
   );
 }
